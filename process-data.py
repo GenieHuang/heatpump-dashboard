fd944915d403589afbe7db50cb5d7f3822ac25bd
GenieHuang <jingyi.huang02@gmail.com>
2024-03-15 16:39:16 -0500

    data process

--- process-data.py
+++ process-data.py
@@ -1,7 +1,9 @@
 import pandas as pd
+
 df = pd.read_csv('./data-raw/uscities.csv')
 
 def data_process(uscities: pd.DataFrame) -> pd.DataFrame:
+    # Create the city_state column by merging city and state_name columns
     uscities['city_state'] = uscities['city'] + ',' + uscities['state_name']
     return uscities[['city_state','lat','lng']]
 
