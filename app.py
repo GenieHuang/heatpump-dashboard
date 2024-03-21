5753441341446367708f36259af15b2e6189183d
GenieHuang <jingyi.huang02@gmail.com>
2024-03-20 21:10:27 -0500

    forecast plot and table

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ import requests_cache
 import pandas as pd
 from retry_requests import retry
 
-import lets_plot as lp
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 import numpy as np
 from shiny import App, Inputs, Outputs, Session, reactive, render, req, ui
 from shinywidgets import render_widget, output_widget
 
 from ipyleaflet import Map,Marker
 from ipywidgets import Layout
+
+from prophet import Prophet
 
 # Requesting histrical weather API
 def historical_weather(lat, lng, start_date, end_date, units):
@@ -19,8 +23,7 @@ def historical_weather(lat, lng, start_date, end_date, units):
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
     openmeteo = openmeteo_requests.Client(session = retry_session)
 
-    # Make sure all required weather variables are listed here
-    # The order of variables in hourly or daily is important to assign them correctly below
+    # Request url and params
     url = "https://archive-api.open-meteo.com/v1/archive"
     params = {
         "latitude": lat,               # input
@@ -35,10 +38,6 @@ def historical_weather(lat, lng, start_date, end_date, units):
 
     # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
     # Process daily data. The order of variables needs to be the same as requested.
     daily = response.Daily()
@@ -55,9 +54,6 @@ def historical_weather(lat, lng, start_date, end_date, units):
     daily_dataframe = pd.DataFrame(data = daily_data)
     return daily_dataframe, response.Latitude(),response.Longitude()
 
-# lets-plot setup
-lp.LetsPlot.setup_html()
-
 # create full and tidy data
 cities = pd.read_csv("data/cities.csv")
 
@@ -71,7 +67,7 @@ app_ui = ui.page_sidebar(
         ui.output_text("selected_lat_lng"),
         ui.input_date_range("dates", "Dates", start="2022-01-01", end="2024-01-01",max="2024-01-01",min="2020-01-01"),
         ui.input_numeric("forecast_year", "Years to Forecast", 1, min=1, max=5),
-        ui.input_radio_buttons( "forecast_trend",  "Forecast Trend",  {"1": "Flat", "2": "Linear"}, selected="1"),  
+        ui.input_radio_buttons( "forecast_trend",  "Forecast Trend",  {"flat": "Flat", "linear": "Linear"}, selected="flat"),  
         ui.input_radio_buttons( "units",  "Units",  {"fahrenheit": "Fahrenheit", "celsius": "Celsius"}, selected="fahrenheit"),  
         ui.output_ui("plot_temp_slider"),
         ui.input_checkbox_group("plot_options","Plot Options",choices=["Weekly Rolling Average","Monthly Rolling Average"],inline=False),
@@ -83,11 +79,11 @@ app_ui = ui.page_sidebar(
     ),
     ui.page_navbar(
         ui.nav_panel("Historical",
-                     ui.output_ui("historical_plot"),
+                     ui.output_plot("historical_plot"),
                      ui.hr(),
                      ui.output_data_frame("historical_df")),
         ui.nav_panel("Forecast",
-                     ui.output_ui("forecast_plot"),
+                     ui.output_plot("forecast_plot"),
                      ui.hr(),
                      ui.output_data_frame("forecast_df")),
         ui.nav_panel("About"),
@@ -119,12 +115,14 @@ def server(input: Inputs, output: Outputs, session: Session):
         lng = selected_lat_lng["lng"]
         return lat,lng
     
+    # Coordinates
     @output
     @render.text("selected_lat_lng")
     def selected_lat_lng():
         _, lat, lng = get_data()
         return f"{lat:.4f}°N, {lng:.4f}°E"
     
+    # Map
     @render_widget("map")
     def map():
         _, lat, lng = get_data()
@@ -133,17 +131,19 @@ def server(input: Inputs, output: Outputs, session: Session):
         map.add_layer(marker)
         return map
     
+    # Historical table
     @render.data_frame("historical_df") 
     def historical_df():
-        return render.DataGrid(calculate_table(),summary=False,height='3000px',width=1600)
+        return render.DataGrid(calculate_historical_table(),summary=False,height='3000px',width=1600)
     
+    # Calculate the historical table
     @reactive.Calc
-    def calculate_table():
+    def calculate_historical_table():
         selected_temp = input.table_temp()
         temp_list = list(range(selected_temp[1], selected_temp[0] - 1, -1))
         historical_rows = []
         daily_dataframe,_,_ = get_data()
-        temp_table = daily_dataframe[['temperature_2m_min']].astype(float)
+        temp_table = daily_dataframe[["temperature_2m_min"]].astype(float)
         
         for temp in temp_list:
                 days_below = temp_table[temp_table["temperature_2m_min"] < temp].shape[0]
@@ -157,6 +157,7 @@ def server(input: Inputs, output: Outputs, session: Session):
         historical_df = pd.DataFrame(historical_rows)
         return historical_df
     
+    # Plot slider
     @output
     @render.ui("plot_temp_slider")
     def plot_temp_slider():
@@ -166,6 +167,7 @@ def server(input: Inputs, output: Outputs, session: Session):
         else:  # Celsius
             return ui.input_slider("plot_temp", "Plot Temperature", min=-25, max=10, step=1, value=-15)
     
+    # Table slider
     @output
     @render.ui("table_temp_slider")
     def table_temp_slider():
@@ -175,9 +177,12 @@ def server(input: Inputs, output: Outputs, session: Session):
         else:  # Celsius
             return ui.input_slider("table_temp", "Table Temperature", min=-30, max=15, step=1, value=[-20,-10])
 
+    # Historical plot
     @output
-    @render.ui("historical_plot")
+    @render.plot(alt="historical_plot")
+    # @render.ui("historical_plot")
     def historical_plot():
+
         daily_dataframe,_,_ = get_data()
         daily_dataframe["date"] = pd.to_datetime(daily_dataframe["date"])
 
@@ -185,38 +190,116 @@ def server(input: Inputs, output: Outputs, session: Session):
         daily_dataframe["monthly_rolling"] = daily_dataframe["temperature_2m_min"].rolling(window=30).mean()
 
         selected_temp_line = input.plot_temp()
-
-        date_range = pd.date_range("2020-01-01","2024-01-01", freq="3M")
-        breaks_list = date_range.tolist()
-
         plot_options = input.plot_options()
+        
+        fig, ax = plt.subplots(figsize=(14, 5))
 
-        p = (
-            lp.ggplot(daily_dataframe, lp.aes(x="date"))
-            + lp.geom_point(lp.aes(y="temperature_2m_min"), color="grey", alpha=0.5) 
-            + lp.geom_point(lp.aes(y="temperature_2m_min"), 
-                    color="black",
-                    data=daily_dataframe[daily_dataframe['temperature_2m_min'] > selected_temp_line])
+        ax.scatter(daily_dataframe["date"], daily_dataframe["temperature_2m_min"], color="grey", alpha=0.8, s=10)
 
-            + lp.geom_hline(yintercept=selected_temp_line, color="black", size=0.5, show_legend=False)
-            + lp.ggsize(width = 1400, height = 500)
-            + lp.labs(x="",y="Daily Minimum Temperature °F")
-            + lp.scale_x_datetime(breaks=breaks_list,format="%Y-%m")
-            + lp.theme(panel_border=lp.element_rect(color="black",size=1),
-                       axis_title=lp.element_text(face="bold"),
-                       axis_text_x = lp.element_text(angle=0))
-        )
+        ax.scatter(daily_dataframe[daily_dataframe['temperature_2m_min'] > selected_temp_line]["date"], 
+               daily_dataframe[daily_dataframe['temperature_2m_min'] > selected_temp_line]["temperature_2m_min"], 
+               color="black", s=10)
+        
+        # Horizontal line for the selected temperature
+        ax.axhline(y=selected_temp_line, color="grey", linewidth=0.5)
 
+        # Weekly and monthly rolling averages
         if "Weekly Rolling Average" in plot_options:
-            p += lp.geom_line(lp.aes(y="weekly_rolling"), color = "orange", size = 1)
+            ax.plot(daily_dataframe["date"], daily_dataframe["weekly_rolling"], color="darkorange", linewidth=2)
 
         if "Monthly Rolling Average" in plot_options:
-            p += lp.geom_line(lp.aes(y="monthly_rolling"), color = "blue", size = 1)
-            
-        phtml = lp._kbridge._generate_static_html_page(p.as_dict(), iframe=True)
+            ax.plot(daily_dataframe["date"], daily_dataframe["monthly_rolling"], color="cornflowerblue", linewidth=2)
 
-        # Return the HTML to be rendered in the Shiny app
-        return ui.HTML(phtml)
+        # Formatting the axis
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
 
+        ax.set_ylabel('Daily Minimum Temperature °F')
+        ax.legend()
+
+        plt.xticks(rotation=0)
+        plt.grid(True,alpha = 0.5)
+        plt.tight_layout()
+
+        return fig
+
+    def get_forecast_df_fig():
+        daily_dataframe,_,_ = get_data()
+        daily_dataframe.rename(columns={"date":"ds","temperature_2m_min":"y"},inplace = True)
+        daily_dataframe['ds'] = daily_dataframe['ds'].dt.tz_localize(None)
+
+        forecast_trend = input.forecast_trend()
+        forecast_year = input.forecast_year()
+
+        end_date = pd.to_datetime(input.dates()[1])
+
+        # Forcasting
+        m = Prophet(growth = forecast_trend)
+        m.fit(daily_dataframe)
+
+        future = m.make_future_dataframe(periods=(365*int(forecast_year)))
+
+        forecast = m.predict(future)
+        future_dataframe = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+
+        forecast['ds'] = pd.to_datetime(forecast['ds'])
+
+        # Split the DataFrame based on the 'end_date'
+        forecast_plot_df = forecast[forecast['ds'] > end_date]
+
+        fig = m.plot(forecast_plot_df)
+
+        return future_dataframe, fig
+    
+    # Calculate the forecasting table
+    @reactive.Calc
+    def calculate_forecast_table():
+
+        future_dataframe,_ = get_forecast_df_fig()
+
+        temp_table = future_dataframe[["yhat_lower"]].astype(float)
+
+        selected_temp = input.table_temp()
+        temp_list = list(range(selected_temp[1], selected_temp[0] - 1, -1))
+        forecast_rows = []
+
+        for temp in temp_list:
+                days_below = temp_table[temp_table["yhat_lower"] < temp].shape[0]
+                proportion_below = round(days_below / temp_table.shape[0],3)
+                forecast_rows.append({
+                    "Temp": temp,
+                    "Days Below": days_below,
+                    "Proportion Below": proportion_below
+                })
+
+        forecast_df = pd.DataFrame(forecast_rows)
+        return forecast_df
+    
+    # Forecasting table
+    @render.data_frame("forecast_df") 
+    def forecast_df():
+        return render.DataGrid(calculate_forecast_table(),summary=False,height='3000px',width=1600)
+
+    # Forecasting plot
+    @output
+    @render.plot(alt="forecast_plot")
+    def forecast_plot():
+
+        _, fig = get_forecast_df_fig()
+        ax = fig.gca()
+
+        selected_temp_line = input.plot_temp()
+
+        daily_dataframe,_,_ = get_data()
+        daily_dataframe["date"] = pd.to_datetime(daily_dataframe["date"])
+
+        ax.scatter(daily_dataframe["date"], daily_dataframe["temperature_2m_min"], color="black", s=10)
+
+        # Horizontal line for the selected temperature
+        ax.axhline(y=selected_temp_line, color="grey", linewidth=0.5)
+
+        plt.ylabel("Daily Minimum Temperature °F")
+        plt.xlabel("")
+        return fig
 # run app
 app = App(app_ui, server)
